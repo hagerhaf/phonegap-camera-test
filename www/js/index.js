92b998c61dbfb473a91d561b4a5f7fccec0e7fd0
rod <rod@roddickinson.net>
2017-11-05 16:45:46 +0000

    updated index.html

--- www/js/index.js
+++ www/js/index.js
@@ -8,21 +8,25 @@ document.addEventListener('deviceready', function() {
     listeningElement.innerHTML = "device ready";
     listeningElement.style.backgroundColor = "#01DA04";
 
-    //launch camera
+    //on click of button launch camera
     document.getElementById('camera-btn').addEventListener('click', takePic);
 });
 
+
+//camera functions:
+//activate camera, take picture, then trigger onSuccess or onFail callback functions
 function takePic() {
     console.log('click');
     navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });
 }
 
-
+//Do something with the captured photo
 function onSuccess(imageURI) {
     var image = document.getElementById('myImage');
     image.src = imageURI;
 }
 
+//Alert that the photo was not captured
 function onFail(message) {
     alert('Failed because: ' + message);
 }
